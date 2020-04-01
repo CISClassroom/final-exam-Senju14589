43670ecfd7fec8036e8bacfacbea1ef4be429886
Senju14589 <59078952+Senju14589@users.noreply.github.com>
2020-04-01 15:22:21 +0700

    OOPFinal_623410008-8

---
+++ OOPFinal_623410008-8/src/com/company/Main.java
@@ -0,0 +1,36 @@
+package com.company;
+
+public class Main {
+    String name = "";
+    String id = " ";
+    String nickname =" ";
+    String stphone =" ";
+    String location1 = " ";
+    String location2 = " ";
+    String location3 = " ";
+    String lcphone = " ";
+    String pname = "";
+    String plastname = "";
+    String pnickname = "";
+    String pphone = "";
+
+
+        public void GetStudent()
+        {
+            System.out.println("ชื่อ-สกุล : " + name);
+            System.out.println("รหัสนักศึกษา : " + id);
+            System.out.println("อายุ " + nickname );
+            System.out.println("สาขาวิทยาการคอมพิวเตอร์และสารสนเทศ");
+            System.out.println("เบอร์โทรศัพท์" + stphone);
+            System.out.println("สถานที่ฝึกงาน" + location1);
+            System.out.println("ที่อยู่" + location2);
+            System.out.println("เบอร์โทรศัพท์" + lcphone);
+            System.out.println("เจังหวัดที่ตั้ง์" + location3);
+            System.out.println("พี่เลี้บง");
+            System.out.println("ชื่อ" + pname);
+            System.out.println("นามสกุล" + plastname);
+            System.out.println("ชื่อเล่น" + pnickname);
+            System.out.println("เบอร์โทรศัพท์" + pphone);
+        }
+    }
+}
